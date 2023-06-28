8cf2909aa238fc48636b54efeae63bcea799ac4b
Illia Kornyk <krekerbrow@gmail.com>
2023-06-28 21:53:13 +0300

    feat: add `Sign in` button

--- src/components/auth/sign-in-form/index.tsx
+++ src/components/auth/sign-in-form/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Typography } from '@mui/material';
+import { Box, Typography, Button } from '@mui/material';
 import React from 'react';
 import Input from '../../TextFields/TextField';
 import PasswordInput from '../../TextFields/PasswordField';
@@ -28,6 +28,25 @@ const SignInForm: React.FC<SignInFormProps> = ({ title, subtitle }) => {
         />
 
         <PasswordInput />
+
+        <Button
+          variant="contained"
+          sx={{
+            backgroundColor: 'hsla(91, 78%, 33%, 1)',
+            boxShadow:
+              '0px 1px 5px 0px hsla(0, 0%, 0%, 0.12), 0px 2px 2px 0px hsla(0, 0%, 0%, 0.14), 0px 3px 1px -2px hsla(0, 0%, 0%, 0.2)',
+            borderRadius: 0,
+            padding: '10px 16px',
+            textTransform: 'none',
+            fontSize: '16px',
+            fontWeight: '600',
+            '&:hover': {
+              backgroundColor: 'hsla(91, 78%, 33%, 0.8)',
+            },
+          }}
+        >
+          Sign In
+        </Button>
       </Box>
     </div>
   );
