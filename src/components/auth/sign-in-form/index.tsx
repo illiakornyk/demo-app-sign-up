ec9869f5459e9b58214170ea2485e685bc45338c
Illia Kornyk <krekerbrow@gmail.com>
2023-06-28 22:11:48 +0300

    feat: add paragraph below `sign in` button

--- src/components/auth/sign-in-form/index.tsx
+++ src/components/auth/sign-in-form/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Typography, Button } from '@mui/material';
+import { Box, Typography, Button, Link } from '@mui/material';
 import React from 'react';
 import Input from '../../TextFields/TextField';
 import PasswordInput from '../../TextFields/PasswordField';
@@ -47,6 +47,29 @@ const SignInForm: React.FC<SignInFormProps> = ({ title, subtitle }) => {
         >
           Sign In
         </Button>
+
+        <Typography
+          sx={{
+            fontFamily: 'Montserrat',
+            fontSize: '12px',
+            fontWeight: '400',
+            lineHeight: '19px',
+            letterSpacing: '0em',
+            textAlign: 'center',
+            color: 'hsla(120, 4%, 95%, 1)',
+          }}
+        >
+          Don’t have account yet?{' '}
+          <Link
+            href="/sign-up"
+            sx={{
+              color: 'hsla(217, 79%, 72%, 1)',
+              textDecoration: 'none',
+            }}
+          >
+            New Account
+          </Link>
+        </Typography>
       </Box>
     </div>
   );
