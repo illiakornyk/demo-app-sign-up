f28f37672593b9a0c19004d852174a5598b1f139
Illia Kornyk <krekerbrow@gmail.com>
2023-06-29 20:33:01 +0300

    feat: add some components to `Home` page
    separate the button component for `formButton`

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,68 @@
+import { Box, Typography } from '@mui/material';
 import React from 'react';
+import Header from '../../components/header';
+import { Container } from '@mui/system';
+import { CongratulationsIcon } from '../../components/icons/congratulations';
+import FormButton from '../../components/buttons/formButton';
 
 function HomePage() {
   return (
-    <div>
-      <h1>Home Page</h1>
-      {/* Add your home page content here */}
-    </div>
+    <Box
+      sx={{
+        bgcolor: 'hsla(217, 33%, 17%, 1)',
+        display: 'flex',
+        flexDirection: 'column',
+        padding: '3rem 3.75rem',
+        minHeight: '100vh',
+      }}
+    >
+      <Header title="InCode" subtitle="Finance" />
+
+      <Container
+        maxWidth="sm"
+        sx={{
+          textAlign: 'center',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          gap: '3rem',
+          marginTop: '6.8rem',
+        }}
+      >
+        <Typography
+          variant="h2"
+          color="white"
+          sx={{
+            textTransform: 'uppercase',
+            fontWeight: 700,
+            fontSize: '3rem',
+            position: 'relative',
+          }}
+        >
+          Congratulations
+          <Box
+            sx={{
+              position: 'absolute',
+              top: '-210%',
+              right: '-22%',
+            }}
+          >
+            <CongratulationsIcon />
+          </Box>
+        </Typography>
+        <Typography
+          color="white"
+          sx={{
+            fontWeight: 600,
+            fontSize: '1rem',
+          }}
+        >
+          Now you are on the main page. Soon we will provide you with detailed
+          feedback on the result of your work
+        </Typography>
+        <FormButton>Log Out</FormButton>
+      </Container>
+    </Box>
   );
 }
 
